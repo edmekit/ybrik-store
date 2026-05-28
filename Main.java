3404a16b49ce3c3bfb7d027e8ea70b1b3a1f9f18
DIPHIDIMI <ddmijares1@up.edu.ph>
2026-05-28 17:38:31 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-
-    }
-}
-
-public class HelloWorld {
-    public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 }
