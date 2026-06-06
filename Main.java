ac0e92dd2ee7c3228ebcc61dfbcbd9c68a968512
edmekit <mekitpekit.eggmarc@gmail.com>
2026-06-06 12:26:10 +0800

    added saveload file mthods

--- Main.java
+++ Main.java
@@ -1,14 +1,17 @@
-import java.util.*;
+import java.util.*;  
+import java.io.*; // for file handling
 
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);       // scanner object
-        List<Products> inventory = new ArrayList<>();   // list of Products objects
+        File file = new File("Test.txt");
+        List<Products> inventory = loadProducts(file);   // load products
 
         mainMenu(scanner, inventory);
 
+        saveProducts(inventory);                         // save products after choosing exit  
         scanner.close();
     }
 
@@ -30,7 +33,7 @@ public class Main {
             switch (choice) {
                 case 1 -> addItem(scanner, inventory);
                 case 2 -> editMenu(scanner, inventory);
-                // case "3" -> deleteItem(scanner);
+                case 3 -> deleteItem(scanner, inventory);
                 case 4 -> listItems(inventory); 
                 case 5 -> { return; }  // exit the loop
                 default  -> System.out.println("\nInvalid option. Please enter 1-5.\n");
@@ -51,6 +54,20 @@ public class Main {
             }
         }
         return null;
+    }
+
+    private static void deleteItem(Scanner scanner, List<Products> inventory) {
+        System.out.print("\nEnter name of product to delete: ");
+        String name = scanner.nextLine();
+
+        Products product = getProduct(inventory, name);
+
+        if (product != null) {
+            inventory.remove(product);
+            System.out.println("\nItem deleted successfully!\n");
+        } else {
+            System.out.println("\nProduct not found.\n");
+        }
     }
 
     private static void addItem(Scanner scanner, List<Products> inventory) {
@@ -94,4 +111,47 @@ public class Main {
         }
     }
 
+    private static List<Products> loadProducts(File file) {
+        List<Products> inventory = new ArrayList<>();
+        try {
+            Scanner fileread = new Scanner(file);
+
+            while (fileread.hasNextLine()){                             // while there is a line
+                String line = fileread.nextLine();                      // read a line
+                String[] parts = line.split(",");                // split the line based on the regex   
+                String name = parts[0];
+                double price = Double.parseDouble(parts[1]);
+                int quantity = Integer.parseInt(parts[2]);
+                Products product = new Products(name, price, quantity); // make a new Products object
+                inventory.add(product);                                 // add to the list
+            }
+            fileread.close();
+            return inventory;
+        }
+
+        catch (FileNotFoundException e) {                       // error handling of Java
+            System.out.println("File not found.");
+            return inventory;
+        }
+
+    }
+
+    private static void saveProducts(List<Products> inventory) {
+        try {
+            FileWriter filewrite = new FileWriter("Test.txt");
+            for (int i = 0; i < inventory.size(); i++) {
+                Products product = inventory.get(i);                        // get product to write
+                String name = product.getName();                            
+                double price = product.getPrice();
+                int quantity = product.getQuantity();
+                String line = name + "," + price + "," + quantity + "\n";   // template to write to file
+                filewrite.write(line);   
+            }
+            filewrite.close();
+
+        } catch (IOException e) {                                       // error handling   
+            System.out.println("An error occurred while writing to the file.");
+            }
+  
+    }
 }
